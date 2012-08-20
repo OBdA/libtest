eedbb0d35e93849040dd7b91d4c6503489b90129
Olaf Ohlenmacher <olf@obda.de>
2012-08-20 21:34:46 +0200

    added shell bang and fixed preamble

--- libtest.sh
+++ libtest.sh
@@ -1,22 +1,23 @@
+#! /bin/sh
 # libtest.sh
-# Bash library for testing (Ideas from Perl test modules)
+# Shell library for testing (Ideas from Perl's Test::Harness)
 #
 #..    Copyright 2011,2012 Olaf Ohlenmacher
 #..
-#..    This file is part of libsh.
+#..    This file is part of libtest.
 #..
-#..    libsh is free software: you can redistribute it and/or modify
+#..    libtest is free software: you can redistribute it and/or modify
 #..    it under the terms of the GNU General Public License as published by
 #..    the Free Software Foundation, either version 3 of the License, or
 #..    (at your option) any later version.
 #..
-#..    libsh is distributed in the hope that it will be useful,
+#..    libtest is distributed in the hope that it will be useful,
 #..    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #..    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #..    GNU General Public License for more details.
 #..
 #..    You should have received a copy of the GNU General Public License
-#..    along with libsh.  If not, see <http://www.gnu.org/licenses/>.
+#..    along with libtest.  If not, see <http://www.gnu.org/licenses/>.
 #..
 #..
 
