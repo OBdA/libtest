047f119560b1a7228a920b0f0db1cb10b2bf3beb
Olaf Ohlenmacher <olf@obda.de>
2013-10-16 21:32:13 +0200

    updated copyright

--- libtest.sh
+++ libtest.sh
@@ -2,7 +2,7 @@
 # libtest.sh
 # Shell library for testing (Heavily inspired from Perl's Test::Harness)
 #
-#..    Copyright 2011,2012 Olaf Ohlenmacher
+#..    Copyright 2011,2012,2013 Olaf Ohlenmacher
 #..
 #..    This file is part of libtest.
 #..
@@ -927,7 +927,7 @@ __END__ ()
 #..    Written by Olaf Ohlenmacher
 #..
 #..COPYRIGHT
-#..    Copyright 2011,2012 Olaf Ohlenmacher.  License GPLv3+: GNU GPL
+#..    Copyright 2011,2012,2013 Olaf Ohlenmacher.  License GPLv3+: GNU GPL
 #..    version 3 or later <http://gnu.org/licenses/gpl.html>. This is free
 #..    software: you are free to change and redistribute it. There is NO
 #..    WARRANTY, to the extent permitted by law.
